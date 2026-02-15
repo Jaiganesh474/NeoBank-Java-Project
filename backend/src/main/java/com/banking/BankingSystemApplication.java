062dbe00d72d1e850ecb557d514e71b39cda68de
Jaiganesh474 <jaiganeshrio474@gmail.com>
2026-02-15 23:54:46 +0530

    Force IPv4 preference programmatically to resolve Render networking timeouts

--- backend/src/main/java/com/banking/BankingSystemApplication.java
+++ backend/src/main/java/com/banking/BankingSystemApplication.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 public class BankingSystemApplication {
 
     public static void main(String[] args) {
+        System.setProperty("java.net.preferIPv4Stack", "true");
         SpringApplication.run(BankingSystemApplication.class, args);
     }
 }
